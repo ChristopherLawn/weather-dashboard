438815e09cec26a4e66c5429f38ef9115a412951
CLAW <christopher.lawn@yahoo.com>
2021-12-15 23:16:35 -0800

    fixed bug so that buttons and city names will stop repeating

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,33 @@
-var searchInputEl = document.querySelector("#cityname");
-var searchCity = document.querySelector("#user-form");
-var searchContainerEl = document.querySelector("#city-container");
+var searchCity = document.querySelector("#user-search");
+var citySearchEl = document.querySelector("#cityname");
+var citySearchContainerEl = document.querySelector("#city-list");
+var citySearchTerm = document.querySelector("#city-search-term");
+var citySearchArray = [];
+function removeDuplicates(citySearchArray) {
+    let unique = [];
+    citySearchArray.forEach(element => {
+        if (!unique.includes(element)) {
+            unique.push(element)
+        }
+    })
+    return unique;
+}
+// console.log(removeDuplicates(citySearchArray));
 
 var citySubmitHandler = function(event) {
     // prevent page from refreshing
     event.preventDefault();
 
-    // // get value from input element
-    var cityName = searchInputEl.value.trim();
+    // get value from input element
+    var cityName = citySearchEl.value.trim();
     console.log(cityName);
 
     if (cityName) {
         findCity(cityName);
 
         // clear old content
-        searchContainerEl.textContent = "";
-        searchInputEl.value = "";
+        // citySearchContainerEl.textContent = "";
+        citySearchEl.value = "";
     } else {
         alert("Please enter a city name");
     }
@@ -34,11 +46,95 @@ var findCity = function(cityInput) {
             console.log(response);
             response.json().then(function(data) {
                 console.log(data);
+                displayCities(data.name);
             });
         } else {
             alert('Error: City Not Found!  Please use format "City" (E.g. Portland) or "City, State" (E.g. Portland, OR) or "City, State, Country" (E.g. Portland, OR, US)');
         }
+    })
+    .catch(function(error) {
+        alert("Unable to connect to Weather Dashboard");
     });
 };
+
+var displayCities = function(cityNameButton) {
+//     // create user search history array
+//     // if (cities.length === 0) {
+//     //     citySearchContainerEl.textContent = "No cities found.";
+//     //     return;
+//     // }
+
+//     // citySearchTerm.textContent = searchTerm;
+
+//     // loop over repos
+//     // for (var i = 0; i < cities.length; i++) {
+//     //     // format repo name
+//     //     var cityName = cities[i];
+
+//         // var cityName = citySearchEl.value.trim();
+//         document.body.appendChild(citySearchContainerEl)
+
+        // city search array
+        // var cityCounter = 0;
+        // for (i = 0; i < citySearchArray.length; i++) {
+        //     if (citySearchArray[i] === cityNameButton) {
+        //         cityCounter++
+        //     }
+        // };
+        // if (cityCounter === 0) {
+            let inArray = false;
+            for(let i = 0; i < citySearchArray.length; i++){
+                if(citySearchArray[i] === cityNameButton){
+                    inArray = true;
+                }
+            }
+            if(!inArray){
+                citySearchArray.push(cityNameButton);
+                var cityEl = document.createElement("li");
+                cityEl.classList = "btn btn:hover col-lg-3 col-md-3 col-sm-12";
+                cityEl.textContent = cityNameButton;
+                cityEl.addEventListener("click", function(event) {
+                findCity(event.target.textContent)
+                });
+                citySearchContainerEl.appendChild(cityEl);
+                localStorage.setItem("city-list", cityNameButton);
+                // localStorage.setItem("ul", cityNameButton);
+                console.log(cityEl);
+                console.log(citySearchArray);
+            }
+            
+        // }
+        
+        // create button for each city
+
+        
+
+//         // // create a span element to hold repository name
+//         // var titleEl = document.createElement("span");
+//         // titleEl.textContent = repoName;
+
+//         // append to container
+//         // cityEl.appendChild("#city-list-container");
+
+//         // // create a status element
+//         // var statusEl = document.createElement("span");
+//         // statusEl.classList = "flex-row align-center";
+
+//         // // check if current repo has issues or not
+//         // if (repos[i].open_issues_count > 0) {
+//         //     statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+//         // } else {
+//         //     statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+//         // }
+        
+//         // // append to container
+//         // repoEl.appendChild(statusEl);
+
+//         // append container to the DOM
+        
+//     // };
+};
+
+
 
 searchCity.addEventListener("submit", citySubmitHandler);
