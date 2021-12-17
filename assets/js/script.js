40e1213611599c99fa23dc4a4ebc9812059dbde7
CLAW <christopher.lawn@yahoo.com>
2021-12-17 12:43:34 -0800

    added localStorage save function, added CSS style classes, adjusted HTML file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,22 @@ var searchCity = document.querySelector("#user-search");
 var citySearchEl = document.querySelector("#cityname");
 var citySearchContainerEl = document.querySelector("#city-list");
 var citySearchTerm = document.querySelector("#city-search-term");
-var citySearchArray = [];
-function removeDuplicates(citySearchArray) {
-    let unique = [];
+var citySearchArray;
+if (localStorage.getItem("city-list")) {
+    citySearchArray = JSON.parse(localStorage.getItem("city-list"));
     citySearchArray.forEach(element => {
-        if (!unique.includes(element)) {
-            unique.push(element)
-        }
-    })
-    return unique;
-}
-// console.log(removeDuplicates(citySearchArray));
+        var cityEl = document.createElement("li");
+        cityEl.classList = "btn btn:hover col-lg-3 col-md-3 col-sm-12";
+        cityEl.textContent = element;
+        cityEl.addEventListener("click", function(event) {
+        findCity(event.target.textContent)
+        });
+        citySearchContainerEl.appendChild(cityEl);
+    });
+} else {
+    citySearchArray = [];
+};
+
 
 var citySubmitHandler = function(event) {
     // prevent page from refreshing
@@ -26,18 +31,16 @@ var citySubmitHandler = function(event) {
         findCity(cityName);
 
         // clear old content
-        // citySearchContainerEl.textContent = "";
         citySearchEl.value = "";
     } else {
         alert("Please enter a city name");
     }
 };
 
+
 var findCity = function(cityInput) {
     // format the github api url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+cityInput+",,&appid=e843da64771fc78ef6dbf9ad46b3939d";
-
-    // var apiURL = "https://api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={API key}"
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+cityInput+",,&units=imperial&appid=e843da64771fc78ef6dbf9ad46b3939d";
   
     // make a request to the url
     fetch(apiUrl)
@@ -45,8 +48,15 @@ var findCity = function(cityInput) {
         if (response.ok) {
             console.log(response);
             response.json().then(function(data) {
-                console.log(data);
+                fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+data.coord.lat+"&lon="+data.coord.lon+"&units=imperial&appid=e843da64771fc78ef6dbf9ad46b3939d")
+                .then(function(response) {
+                    if (response.ok) {
+                        response.json().then(function(additionaldata) {
+                console.log(additionaldata);
                 displayCities(data.name);
+                showCityWeather(data, additionaldata);
+                })};
+            });
             });
         } else {
             alert('Error: City Not Found!  Please use format "City" (E.g. Portland) or "City, State" (E.g. Portland, OR) or "City, State, Country" (E.g. Portland, OR, US)');
@@ -57,7 +67,62 @@ var findCity = function(cityInput) {
     });
 };
 
+
 var displayCities = function(cityNameButton) {
+            let inArray = false;
+            for(let i = 0; i < citySearchArray.length; i++){
+                if(citySearchArray[i] === cityNameButton){
+                    inArray = true;
+                }
+            }
+            if(!inArray){
+                citySearchArray.push(cityNameButton);
+                var cityEl = document.createElement("li");
+                cityEl.classList = "btn btn:hover col-lg-3 col-md-3 col-sm-12";
+                cityEl.textContent = cityNameButton;
+                cityEl.addEventListener("click", function(event) {
+                findCity(event.target.textContent)
+                });
+                citySearchContainerEl.appendChild(cityEl);
+                localStorage.setItem("city-list", JSON.stringify(citySearchArray));
+                console.log(cityEl);
+                console.log(citySearchArray);
+            }
+};
+
+
+var showCityWeather = function(data, weatherData) {
+var currentCityWeather = document.querySelector("#current-weather-container");
+currentCityWeather.innerHTML = `<h1>${data.name}</h1>
+    <p>Temp: ${(weatherData.current.temp)}°F</p>
+    <p>Humidity: ${(weatherData.current.humidity)}%</p>
+    <p>Wind Speed: ${(weatherData.current.wind_speed)} MPH</p>
+    <p>UVI Index: ${(weatherData.current.uvi)}</p>` 
+} 
+
+
+searchCity.addEventListener("submit", citySubmitHandler);
+
+
+
+// var citySearchArrayJson = JSON.parse(citySearchArray);
+// citySearchArray = ["boston", "miami", "portland"]
+
+// localStorage.setItem("city-list", JSON.stringify(citySearchArray));
+
+// function removeDuplicates(citySearchArrayParameter) {
+//     let unique = [];
+//     citySearchArrayParameter.forEach(element => {
+//         if (!unique.includes(element)) {
+//             unique.push(element)
+//         }
+//     })
+//     return unique;
+// }
+// console.log(removeDuplicates(citySearchArray));
+
+// var apiURL = "https://api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={API key}"
+
 //     // create user search history array
 //     // if (cities.length === 0) {
 //     //     citySearchContainerEl.textContent = "No cities found.";
@@ -82,30 +147,10 @@ var displayCities = function(cityNameButton) {
         //     }
         // };
         // if (cityCounter === 0) {
-            let inArray = false;
-            for(let i = 0; i < citySearchArray.length; i++){
-                if(citySearchArray[i] === cityNameButton){
-                    inArray = true;
-                }
-            }
-            if(!inArray){
-                citySearchArray.push(cityNameButton);
-                var cityEl = document.createElement("li");
-                cityEl.classList = "btn btn:hover col-lg-3 col-md-3 col-sm-12";
-                cityEl.textContent = cityNameButton;
-                cityEl.addEventListener("click", function(event) {
-                findCity(event.target.textContent)
-                });
-                citySearchContainerEl.appendChild(cityEl);
-                localStorage.setItem("city-list", cityNameButton);
-                // localStorage.setItem("ul", cityNameButton);
-                console.log(cityEl);
-                console.log(citySearchArray);
-            }
-            
-        // }
+
+// }
         
-        // create button for each city
+// create button for each city
 
         
 
@@ -133,8 +178,28 @@ var displayCities = function(cityNameButton) {
 //         // append container to the DOM
         
 //     // };
-};
 
+// var cityEl = document.createElement("li");
+// cityEl.classList = "btn btn:hover col-lg-3 col-md-3 col-sm-12";
+// cityEl.textContent = cityNameButton;
+// cityEl.addEventListener("click", function(event) {
+// findCity(event.target.textContent)
+// });
+// citySearchContainerEl.appendChild(cityEl);
 
-
-searchCity.addEventListener("submit", citySubmitHandler);
+// var populateCityList = function() {
+//     // var cityList = JSON.parse(localStorage.getItem("city-list"));
+//     var cityList = localStorage.getItem("city-list");
+//     for (i = 0; i < cityList.length; i++);
+//     if (cityList.length >= 0) {
+//         cityList.forEach((value) => {
+//             var cityEl = document.createElement("li");
+//             cityEl.classList = "btn btn:hover col-lg-3 col-md-3 col-sm-12";
+//             cityEl.textContent = cityNameButton;
+//             cityEl.addEventListener("click", function(event) {
+//             findCity(event.target.textContent)
+//             });
+//             citySearchContainerEl.appendChild(cityEl);
+//         })
+//     }
+// ;}
